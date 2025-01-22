1cc360f20b11b614fdaf8de282fe86e538afbe35
Fernando Donato <fernandodr60@gmail.com>
2025-01-22 18:47:33 -0300

    projeto de página utilizando javascript

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+
+//const animais = document.getElementById("animais");
+//console.log();
+
+//const gridSection = document.getElementsByClassName("grid-section");
+//console.log(gridSection[0]);
+
+//const primeiraLi = document.querySelector("li");
+//console.log(primeiraLi);
+
+//const primeiraUl = document.querySelector("ul");
+//console.log(primeiraUl);
