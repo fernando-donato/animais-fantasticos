ebebad5ced930bad1175b7464ac61e3e2d0a5c2d
Fernando Donato <fernandodr60@gmail.com>
2025-02-03 15:39:21 -0300

    refatorar dropdown menu

--- js/modules/dropdownmenu.js
+++ js/modules/dropdownmenu.js
@@ -1,19 +1,38 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {
-  const dropdownmenus = document.querySelectorAll("[data-dropdown]");
+export default class DropdownMenu {
+  constructor(dropdownmenus, events) {
+    this.dropdownmenus = document.querySelectorAll(dropdownmenus);
+    this.activeClass = "active";
 
-  dropdownmenus.forEach((menu) => {
-    ["touchstart", "click"].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
-    });
-  });
+    if (events === undefined) {
+      this.events = ["touchstart", "click"];
+    } else {
+      this.events = events;
+    }
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
 
-  function handleClick(event) {
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add("active");
-    outsideClick(this, ["touchstart", "click"], () => {
-      this.classList.remove("active");
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove("active");
     });
+  }
+
+  addDropdowMenusEvent() {
+    this.dropdownmenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
+    });
+  }
+  init() {
+    if (this.dropdownmenus.length) {
+      this.addDropdowMenusEvent();
+    }
+    return this;
   }
 }
